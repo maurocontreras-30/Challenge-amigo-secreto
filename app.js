a77132ac5b5c8dc456d9c11578d2d0136b57b7ec
Mauro COntreras <maurocastrox@gmail.com>
2025-09-23 22:21:13 -0300

    Validar entrada de nombre

--- app.js
+++ app.js
@@ -7,4 +7,9 @@ function agregarAmigo(){
     // CAMBIO el id 'nombre-amigo'
     const campoNombre = document.getElementById('nombre-amigo');
     const nombre = campoNombre.value;
+    // Si el campo está vacío, muestra una alerta y detiene la función.
+    if (nombre === '') {
+    alert('Por favor, inserte un nombre.');
+    return; // Sale de la función si la validación falla.
+  }
 }
